07354e620763eafa7af4e3dec2745fb3fd7c6d7e
prateekkaushal7886 <prateekkaushal7886@gmail.com>
2017-08-15 14:42:00 +0530

    add menu bar

--- index.php
+++ index.php
@@ -12,7 +12,14 @@
     .navbar {
       margin-bottom: 0;
       border-radius: 0;
-    }
+    
+        }
+
+.container-fluid
+{
+
+}
+
     
     /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
     .row.content {height: 450px}
@@ -39,30 +46,89 @@
       }
       .row.content {height:auto;} 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @media only screen and (min-width: 1200px) {
+
+.navbar .nav li {
+    display: table-cell;
+    width: 1%;
+    float: none;
+}
+
+.navbar .nav li a {
+    font-weight: bold;
+   
+    border-left: 1px solid rgba(255, 255, 255, .75);
+    border-right: 1px solid rgba(0, 0, 0, .1);
+}
+}
+
+
+img
+{
+margin-left: 40%;
+}
+.head
+{
+  background-color: white;
+}
+.about
+{
+    box-shadow: 10px 10px 5px #888888;
+    border-radius: 50px;
+    background: linear-gradient(white, #D3D8E8);
+  background-repeat: no-repeat;
+}
+h2
+{
+  text-align: center;
+  text-decoration: underline;
+}
+
+
+
+
+
+
+
+
   </style>
 </head>
 <body>
-
+  <div class="container-fluid"></div>
+  <div class="head"> <a  href="#"><img src="sac_logo.png" width=""></a></div>
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
+
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+     
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
+        <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-tasks"></span> Events</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-gift"></span> Initiatives</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-pencil"></span> Publications</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> The Team</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-usd"></span> Sponsors</a></li>
       </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
+     
     </div>
   </div>
 </nav>
@@ -75,6 +141,26 @@
       <p><a href="#">Link</a></p>
     </div>
     <div class="col-sm-8 text-left"> 
+
+  <br />
+  <h2>ABOUT US</h2>
+  <br /><br />
+
+    <div class="about" style="background-color: white;">
+                    <p class="z-depth-1 text_align" style="font-size: 16px;padding: 3.3em 3em">
+                      The Students' Alumni Cell, IIT Kharagpur is a voluntary student body working under the aegis 
+                      of the Dean of Alumni Affairs and International Relations. It strives to bridge the gap between 
+                      IIT Kharagpur and its esteemed alumni community.<br>
+                            <br>
+                       The Students' Alumni Cell continuously endeavours to strengthen the relations of 
+                       the alumni of this Institute and their alma mater. 
+                       It has been instrumental in organising the Annual Alumni Meet, Regional Student Alumni Meet, 
+                       regular guest lectures by distinguished alumni in the campus, publishing the annual 
+                       literary magazine (Yearnings of Yore) & the annual yearbook.  
+                       The Students' Alumni Cell also operates the Student - Alumni Mentorship Programme, My Imprint
+                      (Giving Back - Alumni contribution) and organises Alvida (the annual farewell dinner).
+                    </p>
+                </div>
       
     </div>
     <div class="col-sm-2 sidenav">
