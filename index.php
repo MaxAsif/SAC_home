70ed8df10f24dbca8a5181e979773403750cf81c
MaxAsif <maxasif@gmail.com>
2017-08-15 16:40:49 +0530

    added footer to index

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
   <style>
     /* Remove the navbar's default margin-bottom and rounded borders */ 
     .navbar {
@@ -171,8 +172,39 @@ margin-left: 40%;
   </div>
 </div>
 
+
+
+
+<div class="row" style="color:white;background-color: black">
+  <div class="col-sm-4"><p id="foot"><strong>Office of Alumni Affairs & International Relations,<br>
+                   Indian Institute of Technology, Kharagpur<br>
+                    Pin-721302, West Bengal, India<br>
+                    Ph: 03222-282236 <br>
+                    Email: aao@hijli.iitkgp.ernet.in</strong></p></div>
+  <div class="col-sm-4"><div id="map" class=" col l4"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d784.9332419499943!2d87.30956309721707!3d22.3193820950838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3a63547376e5c22c!2sMain+Building+IIT+KGP!5e0!3m2!1sen!2sin!4v1471677047994" width="400" height="180" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+</div>
+<div class="col-sm-4">
+               <div class="headsoc"><strong>Connect with us on</strong></div> <br>
+               <div class="btn-group btn-group-justified" class="btn-link"> 
+                  <div class="btn-group" >
+                    <button type="button" class="btn btn-link" style="font-size:36px"><a href="http://www.facebook.com/iitkgp.alumnicell?fref=ts&ref=br_tf" class="icoFacebook" class="img-responsive" title="Facebook" style="color: #3e5b98"><i class="fa fa-facebook-square"></i></a></button>
+                  </div>
+
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-link" style="font-size:36px"><a href="http://twitter.com/IITKgpAlumni" class="icoTwitter" class="img-responsive" title="Twitter"><i class="fa fa-twitter-square" style="color: #4da7de"></i></a></button>
+                  </div>
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-link" style="font-size:36px"><a href="https://www.linkedin.com/company-beta/13273135/" class="icolink" class="img-responsive" title="linkedin"><i class="fa fa-linkedin-square" style="color: #3371b7"></i></a></button>
+                  </div>
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-link" style="font-size:36px;"><a href="http://www.youtube.com/channel/UCtnSs1CT59Rd8L6VlOAxzRA" class="icoyoutube" class="img-responsive" title="You Tube"><i class="fa fa-youtube-square" style="color:#e02a20;"></i></a></button>
+                  </div>
+                </div>
+              </div>
+</div>
+
 <footer class="container-fluid text-center">
-  <p>Footer Text</p>
+  &copy; 2016 Students' Alumni Cell
 </footer>
 
 </body>
